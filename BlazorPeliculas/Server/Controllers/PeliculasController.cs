d06661eaffbd212b0685d2ee135c58c625b48614
Percy Leon <percyleon107@gmail.com>
2021-01-02 07:16:28 -0500

    Actualizar 20/01/2021

---
+++ BlazorPeliculas/Server/Controllers/PeliculasController.cs
@@ -0,0 +1,39 @@
+﻿using BlazorPeliculas.Server.Helpers;
+using BlazorPeliculas.Shared.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeliculasController : ControllerBase
+    {
+        private readonly ApplicationDbContext Context;
+        private readonly IAlmacenadorArchivos AlmacenadorArchivos;
+        private readonly string Contenedor = "peliculas";
+
+        public PeliculasController(ApplicationDbContext context, IAlmacenadorArchivos almacenadorArchivos)
+        {
+            this.Context = context;
+            this.AlmacenadorArchivos = almacenadorArchivos;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Pelicula pelicula)
+        {
+            if (!string.IsNullOrEmpty(pelicula.Poster))
+            {
+                var fotoPersona = Convert.FromBase64String(pelicula.Poster);
+                pelicula.Poster = await AlmacenadorArchivos.GuardarArchivo(fotoPersona, ".jpg", Contenedor);
+            }
+
+            Context.Add(pelicula);
+            await Context.SaveChangesAsync();
+            return pelicula.Id;
+        }
+    }
+}
